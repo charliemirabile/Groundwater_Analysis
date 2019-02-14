fd726a645145416f8072effffd6cc23f71ba441f
Charlie Mirabile <charlie.mirabile@gmail.com>
2019-02-14 15:48:37 -0500

    added empty file to begin the code merging/refactoring project

---
+++ arduino/merge_no_power_stuff/merge_no_power_stuff.ino
@@ -0,0 +1,14 @@
+void setup() {
+ //Steps:
+  
+ //init wire, radio, maybe serial?
+ //scan i2c addresses
+ //maybe send some sort of diagnostic report over serial if it is connected?
+}
+
+void loop() {
+  //Check battery; send message if low
+  //Scan all valid i2c addresses and transmit readings or error stuff?
+  //Scan the 6 analogue pins
+  //Go to sleep
+}
---
+++ arduino/merge_no_power_stuff/radio.ino
@@ -0,0 +1,4 @@
+void test(void)
+{
+  Serial.println("testing derp");
+}
