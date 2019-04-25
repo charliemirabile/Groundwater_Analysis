298530c88a1dad7ed28ef40d6742714dcc49d381
charliemirabile <charlie.mirabile@gmail.com>
2019-04-25 16:43:54 -0400

    commit to add changes made to get the code actually working with iSense

--- python/iSense_API_Test.py
+++ python/iSense_API_Test.py
@@ -8,9 +8,9 @@ def postToAppendData(contributionKey,datasetID,data):
     }
     headers = {'content-type':'application/json'}
     request = requests.post('https://isenseproject.org/api/v1/data_sets/append',data=json.dumps(payload),headers=headers)
-    #print(request.url)
-    #print(request.text)
-    
+    print(request)
+    print(request.url)
+    print(request.text)
 
 def getFormattedTimestamp():
     currentTime = time.time()
@@ -22,30 +22,34 @@ def createData():
     global a
     a+=1
     data = {
-            '18062': [a],
-            '18063': [random.randint(0,100)],
-            '18064': ['Hello World!'],
-            '18061': [getFormattedTimestamp()]
+            '18368': [a],
+            '18369': [str(random.randint(0,100))],
+            '18370': ['Hello World!'],
+            '18367': [getFormattedTimestamp()]
         } 
     return data
 
 def createEmptyDataset():
     payload = {
         'title': 'R-Pi dataset',
-        'contribution_key': 'rpi-key',
+        'contribution_key': 'keykey',
         'contributor_name': 'Raspberry Pi',
         'data':{
-            'Number_1': [0],
-            'Number_2': [0],
-            'Text_1': ['first point'],
-            'Timestamp': [getFormattedTimestamp()]
+            '18367': [getFormattedTimestamp()],
+            '18370': ['asdfasdf'],
+            '18365': ['37'],
+            '18366': ['37'],
+            '18368': ['5'],
+            '18369': [5]
         }
     }
     headers = {'content-type':'application/json'}
-    request = requests.post('https://isenseproject.org/api/v1/projects/3645/jsonDataUpload',data=json.dumps(payload),headers=headers)
+    request = requests.post('https://isenseproject.org/api/v1/projects/3651/jsonDataUpload',data=json.dumps(payload),headers=headers)
+    print(request)
     print(request.url)
     print(request.text)
 
 #createEmptyDataset()
-for i in range(0,500):
-    postToAppendData('rpi-key',100235,createData())
+#for i in range(0,500):
+#postToAppendData('keykey',101110,createData())
+createEmptyDataset()
--- python/read_and_send.py
+++ python/read_and_send.py
@@ -3,51 +3,51 @@ import serial,requests,json,time,datetime,sys
 base_station_feather_identifier = '/dev/ttyACM0'
 
 feather_ids_to_address_dictionaries_for_descriptions = {
-	1337:
+	'1337':
 	{
-		7:"battery",
-		101:"apple",
-		102:"banana"
+		'7':"battery",
+		'101':"apple",
+		'102':"banana"
 	},
-	1338:
+	'1338':
 	{
-		7:"battery",
-		101:"test"
+		'7':"battery",
+		'101':"test"
 	}
 }
 
 node_ids_to_position_information = {
-	1337:
+	'1337':
 	{
-		"depth":5,
-		"lat":5,
-		"long":5
+		"depth":'5',
+		"lat":'5',
+		"long":'5'
 	},
-	1338:
+	'1338':
 	{
-		"depth":7,
-		"lat":8,
-		"long":3
+		"depth":'7',
+		"lat":'8',
+		"long":'3'
 	}
 }
 
 
 iSense_field_idenfifiers = {#MUST ADD
 
-	'timestamp' : 18367,
-	'metric' : 18370,
-	'latitude' : 18365,
-	'longitude' : 18366,
-	'depth' : 18368,
-	'reading' : 18369
+	'timestamp' : '18367',
+	'metric' : '18370',
+	'latitude' : '18365',
+	'longitude' : '18366',
+	'depth' : '18368',
+	'reading' : '18369'
 }
 
 
 
 
-iSense_contribution_key = 'rpi-key'
+iSense_contribution_key = 'keykey'
 
-iSense_dataset_id = '' #MUST ADD
+iSense_dataset_id = 101111
 
 def get_formatted_timestamp():
     return datetime.datetime.fromtimestamp(time.time()).strftime('%Y-%m-%d %H:%M:%S')
@@ -60,6 +60,9 @@ def iSense_append_data(contribution_key,dataset_ID,data):
     }
     headers = {'content-type':'application/json'}
     request = requests.post('https://isenseproject.org/api/v1/data_sets/append',data=json.dumps(payload),headers=headers)
+    print(request)
+    print(request.text)
+#    request.text
 
 try:
     base_station_feather = serial.Serial(base_station_feather_identifier,9600)
@@ -83,19 +86,20 @@ while True:
             print(e)
             print('malformed message - wrong number of delimeters')
         try:
-            iSense_feild_identifier = sensor_identifiers_to_iSense_feilds[sensor_identifier]
-        except KeyError as e:
-            print(e)
-            print('unexpected sensor identifier encountered - no corresponding feild in iSense')
-        data = {
-                iSense_field_idenfifiers['timestamp']:[get_formatted_timestamp()],
-		iSense_field_idenfifiers['latitude']:[node_ids_to_position_information[feather_identifier]['lat']],
-		iSense_field_idenfifiers['longitude']:[node_ids_to_position_information[feather_identifier]['long']],
-		iSense_field_idenfifiers['depth']:[node_ids_to_position_information[feather_idetifier]['depth']],
-		iSense_field_idenfifiers['reading']:[reading],
-		iSense_field_idenfifiers['metric']:[feather_ids_to_address_dictionaries_for_descriptions[feather_identifier][sensor_identifier]]
-
-            }
+		data = {
+                	iSense_field_idenfifiers['timestamp']:[get_formatted_timestamp()],
+			iSense_field_idenfifiers['latitude']:[node_ids_to_position_information[feather_identifier]['lat']],
+			iSense_field_idenfifiers['longitude']:[node_ids_to_position_information[feather_identifier]['long']],
+			iSense_field_idenfifiers['depth']:[node_ids_to_position_information[feather_identifier]['depth']],
+			iSense_field_idenfifiers['reading']:[reading],
+			iSense_field_idenfifiers['metric']:[feather_ids_to_address_dictionaries_for_descriptions[feather_identifier][sensor_identifier]]
+            		}
+	except KeyError as e:
+		print(e)
+		print('unexpected sensor identifier encountered - no corresponding feild in iSense')
+	except NameError as e:
+		print(e)
+		print('here')
         try:
             iSense_append_data(iSense_contribution_key,iSense_dataset_id,data)
         except requests.exceptions.RequestException as e:
