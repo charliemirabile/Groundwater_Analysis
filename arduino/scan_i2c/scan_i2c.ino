0371ad5ba28d957a236ba5038cdebbc3ca2130be
Charlie Mirabile <charlie.mirabile@gmail.com>
2019-05-16 15:07:25 -0400

    updated scan i2c to let a user highlight a device with a blinking light. changed the ezo board setup code to make it more user friendly

--- arduino/scan_i2c/scan_i2c.ino
+++ arduino/scan_i2c/scan_i2c.ino
@@ -8,19 +8,39 @@ void setup() {
   Serial.begin(9600);
   while(!Serial)delay(5);
   Wire.begin();
-  Serial.println("Begin.");
-}
-
-void loop() {
-    Serial.println("Scanning...");
+  Serial.println("Scanning...");
   scan_i2c_bus(i2cAddresses,MAXADDRESSES);
-    Serial.println("Found:");
+  Serial.println("Found:");
   for(int i=0;i<MAXADDRESSES;i++)
   {
     if(i2cAddresses[i]!=0)Serial.println(i2cAddresses[i]);
   }
+  Serial.print("Enter the address of a device to make its light blink: ");
+}
 
-  delay(2000);
+void loop() {
+  if(Serial.available())
+  {
+    String line = Serial.readString();
+    Serial.println(line);
+    int addr = line.toInt();
+    if(addr<=7||addr>=120)
+    {
+      Serial.println("Error invalid address, must be an integer bewteen 8 and 119");
+    }
+    else
+    {
+      Wire.beginTransmission(addr);
+      Wire.write("Find");
+      Wire.endTransmission();
+      delay(5000);
+      Wire.beginTransmission(addr);
+      Wire.write(" ");
+      Wire.endTransmission();
+    }
+    Serial.print("Enter the address of a device to make its light blink: ");
+  }
+  delay(100);
 }
 
 void scan_i2c_bus(int* pAddressArray, int max_addresses)
@@ -30,7 +50,8 @@ void scan_i2c_bus(int* pAddressArray, int max_addresses)
   for(address=8;address<120&&next_index_to_fill<max_addresses;address++)//iterate over addresses 8-119 and ensure we dont fill past end of array
   {
     Wire.beginTransmission(address);
-    status_code = Wire.endTransmission(true);
+    Wire.write("derp");
+    status_code = Wire.endTransmission();
     if(status_code==0)
     {
       pAddressArray[next_index_to_fill]=address;
--- arduino/setup_ezo_board/setup_ezo_board.ino
+++ arduino/setup_ezo_board/setup_ezo_board.ino
@@ -4,93 +4,113 @@
 #define TO_EZO_TX 10
 #define EZO_GROUND 9
 
-void double_blink(void)
+void wait_for_any_message(void)
 {
-  digitalWrite(LED_BUILTIN,HIGH);
-  delay(25);
-  digitalWrite(LED_BUILTIN,LOW);
-  delay(200);
-  digitalWrite(LED_BUILTIN,HIGH);
-  delay(25);
-  digitalWrite(LED_BUILTIN,LOW);
+  bool should_proceed=false;
+  while(!should_proceed)
+  {
+    if(Serial.available())
+    {
+      while(Serial.available())Serial.read();
+      should_proceed=true;
+    }
+    else delay(100);
+  }
 }
-
 void ezo_disconnect_ground(void)
 {
-  pinMode(EZO_GROUND,INPUT);
+  pinMode(EZO_GROUND, INPUT);
 }
 
 void ezo_connect_ground(void)
 {
-  pinMode(EZO_GROUND,OUTPUT);
-  digitalWrite(EZO_GROUND,LOW);
+  pinMode(EZO_GROUND, OUTPUT);
+  digitalWrite(EZO_GROUND, LOW);
 }
 
 void setup() {
-  //setup LED pin and ensure LED is off
+  //setup builtin LED pin
   pinMode(LED_BUILTIN,OUTPUT);
-  digitalWrite(LED_BUILTIN,LOW);
 
-  //ensure the ground pin is disconnected
+  //ensure the ground pin is disconnected so EZO is turned off
   ezo_disconnect_ground();
 
   //disconnect RX and TX pins by making them inputs which are hi-z by default
-  pinMode(TO_EZO_RX,INPUT);
-  pinMode(TO_EZO_TX,INPUT);
-
-  //wait a bit before blinking so that the user can tell them apart from the reset lights
-  delay(500);
-  //blink to tell user to hold down button
-  double_blink();
-  delay(300);
-  double_blink();
+  pinMode(TO_EZO_RX, INPUT);
+  pinMode(TO_EZO_TX, INPUT);
 
   //try to begin Serial
   Serial.begin(9600);
-  //wait until serial is ready bc user has opened window so they have time to press button
-  while(!Serial)delay(10);
+  //wait until serial is ready
+  while (!Serial)delay(10);
 
+
+  Serial.println("Hold down button until told to release");
+  Serial.println("Send any message once holding button to proceed");
+  wait_for_any_message();
+  
   Serial.println("Begining Programming");
 
+  //power up while button is pressed to reset device into UART mode
   ezo_connect_ground();
-
-  delay(5000);
-
+  delay(10000);
   ezo_disconnect_ground();
 
-  //tell user button should be released
-  double_blink();
-  delay(300);
-  double_blink();
+  Serial.println("Release Button");
+  Serial.println("Send any message after releasing button to proceed");
+  wait_for_any_message();
 
-  //give time to realease button
-  delay(2000);
 
+  //power up and connect to EZO in UART MOD
   ezo_connect_ground();
-
-  SoftwareSerial ezo_board(TO_EZO_TX,TO_EZO_RX);
+  SoftwareSerial ezo_board(TO_EZO_TX, TO_EZO_RX);
   ezo_board.begin(9600);
+  while (!ezo_board.isListening())delay(10);
 
-  while(!ezo_board.isListening())delay(10);
-  
+
+  //Wait for a user to give a valid address
+  bool has_setup_addr = false;
   Serial.println("Board is ready, Please enter I2C Address");
-  String addr_string = Serial.readString();
-  int addr = addr_string.toInt();//safe - returns zero if unable to convert which is invalid
-  while(addr<8 || addr> 119)
+  Serial.print("Address: ");
+  String addr_string;
+  int addr;
+  while (!has_setup_addr)
   {
-    Serial.println("Please enter address, must be an integer between 8 and 119");
-    addr_string = Serial.readString();
-    addr = addr_string.toInt();
+    if (Serial.available())
+    {
+      addr_string = Serial.readString();
+      Serial.println(addr_string);
+      addr = addr_string.toInt();//safe - returns zero if unable to convert which is invalid
+      if (addr <= 7 || addr >= 120)
+      {
+        Serial.println("Error invalid address, must be an integer bewteen 8 and 119");
+        Serial.print("Address: ");
+      }
+      else
+      {
+        has_setup_addr = true;
+      }
+    }
+    else delay(100);
   }
+  Serial.println("Changing address");
 
+  //Send message to EZO to put into I2C mode with correct address
   String msg = String("I2C,");
   msg.concat(addr_string);
   ezo_board.println(msg);
+  delay(10000);
+  ezo_disconnect_ground();
   Serial.println("Entering Standby");
 }
 
 void loop() {
-  //indicate standby with LED pattern
-  double_blink();
+  digitalWrite(LED_BUILTIN,HIGH);
+  delay(50);
+  digitalWrite(LED_BUILTIN,LOW);
+  delay(200);
+  digitalWrite(LED_BUILTIN,HIGH);
+  delay(50);
+  digitalWrite(LED_BUILTIN,LOW);
   delay(700);
 }
