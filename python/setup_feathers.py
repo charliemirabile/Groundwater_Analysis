685a9962fd1007769397549728dda3a4587efd80
charliemirabile <charlie.mirabile@gmail.com>
2019-05-23 14:39:49 -0400

    more work on python code

--- python/setup_feathers.py
+++ python/setup_feathers.py
@@ -1,10 +1,11 @@
 import json
 
+should_continue = False
 
-print('''What would you like to do?
-1. Read in and modify existing feather configuration
-2. Start new feather configuration and overwrite previous''')
-
-line = input('Please enter the appropriate number to indicate your choice:')
-
-print(line.length)
+while should_continue == False:
+	print('''What would you like to do?
+	1. Read in and modify existing feather configuration
+	2. Start new feather configuration and overwrite previous''')
+	line = input('Please enter the appropriate number to indicate your choice:')
+	if not (len(line) == 1):
+		print('bad')
