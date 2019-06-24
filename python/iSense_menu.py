2085699ba6bf175df30cd9c43c1dfb80d895a82f
Sam Patriquin <samuel_patriquin@student.uml.edu>
2019-06-24 16:00:27 -0400

    More progress on the iSense portion of the menu. Only thing left is editing existing field identifiers

--- python/iSense_menu.py
+++ python/iSense_menu.py
@@ -1,18 +1,68 @@
 import json, os, re
 
-def get_valid_input(prompt_string):
+def get_valid_alphanumeric_input(prompt_string):
 	response = input(prompt_string)
 	while not re.match(r'^([a-zA-Z0-9_\-\(\)])+$', response):
 		print("Invalid input, please use only alphanumeric characters undescores dashes or parentheses.")
 		response = input(prompt_string)
 	return response
 
-def edit_node_level_identifiers(current_config):
+
+def get_valid_numeric_input(prompt_string):
+	response = input(prompt_string)
+	while not re.match(r'^([0-9])+$', response):
+		print("Invalid input, please use only numeric characters.")
+		response = input(prompt_string)
+	return response
+
+def edit_generic_level_identifiers(current_config, type):
 	backup_config = current_config
-	option_dictionary = {'1':'Cancel', '2':'Save and return to previos menu', '3': 'Create new node identifier', '4': 'Edit existing node identifier',
-	{'5': 'Delete existing node identifier'}
+	if type not in current_config['iSense_info']['field_identifiers']:
+		current_config['iSense_info']['field_identifiers'][type] = {}
+	option_dictionary = {'1':'Cancel', '2':'Save and return to previous menu', '3': 'Create new node identifier', '4': 'Edit existing node identifier',
+	'5': 'Delete existing node identifier'}
 	print("What do you want to do?")
-	return current_config
+	print('-------------------------------------------------------------------')
+	while True:
+		for index in sorted(option_dictionary):
+			print('{0}. {1}'.format(index, option_dictionary[index]))
+		selection = input("Please enter the appropriate number to indicate your choice: ")
+		if selection == '1':
+			print('-------------------------------------------------------------------')
+			print('Successfully canceled changed')
+			return backup_config
+		if selection == '2':
+			print('-------------------------------------------------------------------')
+			print('Successfully saved changes')
+			return current_config
+		if selection == '3':
+			print('-------------------------------------------------------------------')
+			field_name = get_valid_alphanumeric_input("Please enter the new "+ type + " field name: ")
+			field_identifier = get_valid_numeric_input("Please enter the new "+ type + " field identifier: ")
+			current_config['iSense_info']['field_identifiers'][type][field_name] = field_identifier
+			print('Successfully added a new ' + type + ' identifier')
+		if selection == '4':
+			print('-------------------------------------------------------------------')
+		if selection == '5':
+			print('-------------------------------------------------------------------')
+			identifier_dictionary = {'1':'Cancel'}
+			for indx, val in enumerate(current_config['iSense_info']['field_identifiers'][type].keys()):
+				identifier_dictionary[str(indx + 2)] = val
+			for indx in sorted(identifier_dictionary):
+				print('{0}. {1}'.format(indx, identifier_dictionary[indx]))
+			choice = get_valid_numeric_input("Which " + type + " identifier would you like to delete: ")
+			if choice == '1':
+				continue
+			if choice in list(identifier_dictionary):
+				ans_string = input('Are you sure you want to delete ' + identifier_dictionary[choice] + ' ? (Y/n): ')
+				ans = ans_string[0].lower()
+				if ans == 'y':
+					print('-------------------------------------------------------------------')
+					print('Successfully deleted ' + identifier_dictionary[choice])
+					del current_config['iSense_info']['field_identifiers'][type][identifier_dictionary[choice]]
+				else:
+					print('-------------------------------------------------------------------')
+					print('Delection canceled')
 
 def edit_field_identifiers(current_config):
 	backup_config = current_config
@@ -35,17 +85,17 @@ def edit_field_identifiers(current_config):
 			print('Successfully saved changes')
 			return current_config
 		if selection == '3':
-			timestamp_identifier = get_valid_input("Please enter the timestamp identifier: ")
+			timestamp_identifier = get_valid_numeric_input("Please enter the timestamp identifier: ")
 			current_config['iSense_info']['field_identifiers']['timestamp_identifier'] = timestamp_identifier
 			print('-------------------------------------------------------------------')
 			print('Successfully changed the timestamp identifier')
 		if selection == '4':
-			reading_identifier = get_valid_input("Please enter the reading identifier: ")
+			reading_identifier = get_valid_numeric_input("Please enter the reading identifier: ")
 			current_config['iSense_info']['field_identifiers']['reading_identifier'] = reading_identifier
 		if selection == '5':
-			edit_node_level_identifiers()
+			edit_generic_level_identifiers(current_config, 'node_level')
 		if selection == '6':
-			edit_sensor_level_identifiers()
+			edit_generic_level_identifiers(current_config, 'sensor_level')
 def edit_iSense_config(current_config):
 	backup_config = current_config
 	if 'iSense_info' not in current_config:
@@ -66,12 +116,12 @@ def edit_iSense_config(current_config):
 			print('Successfully saved changes')
 			return current_config
 		if selection == '3':
-			dataset_id =  get_valid_input("Please enter the iSense dataset ID: ")
+			dataset_id =  get_valid_numeric_input("Please enter the iSense dataset ID: ")
 			current_config['iSense_info']['dataset_id'] = dataset_id
 			print('-------------------------------------------------------------------')
 			print("Successfully changed the dataset ID")
 		if selection == '4':
-			contribution_key = get_valid_input("Please enter the iSense contribution key: ")
+			contribution_key = get_valid_alphanumeric_input("Please enter the iSense contribution key: ")
 			current_config['iSense_info']['contribuition_key'] = contribution_key
 			print('-------------------------------------------------------------------')
 			print("Successfully changed the contribution_key")
