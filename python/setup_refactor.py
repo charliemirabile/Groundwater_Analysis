5dab4a65c54bc7934e814253ae07186ce3d08767
charliemirabile <charlie.mirabile@gmail.com>
2019-06-25 17:17:54 -0400

    one more helper function

--- python/setup_refactor.py
+++ python/setup_refactor.py
@@ -17,16 +17,19 @@ def pick_config_or_cancel(prompt_string):
         if choice in valid_choices: return list_of_configs[int(choice)-2]
         print('-------------------------------------------------------------------')
         print("Error, invalid input.")
-        
+
+
+
 def pick_subdict_or_cancel(subdict, prompt_string):
     list_of_keys = list(subdict.keys())
+    valid_choices = set()
+    for index,name in enumerate(list_of_keys):
+        valid_choices.add(str(index+2))
     print('-------------------------------------------------------------------')
     while True:
         print(prompt_string)
         print('1. Cancel')
-        valid_choices = set()
         for index,name in enumerate(list_of_keys):
-            valid_choices.add(str(index+2))
             print("{0}. {1}".format(index+2,name))
         choice = input('Enter the number corresponding to your selection: ')
         if choice == '1': return 'Cancel'
@@ -35,7 +38,7 @@ def pick_subdict_or_cancel(subdict, prompt_string):
         print("Error, invalid input.")
 
 
-print(pick_subdict_or_cancel({1:True,2:False},"Which cuh do you want?"))
+#print(pick_subdict_or_cancel({1:True,2:False},"Which cuh do you want?"))
 
 def get_confirm(name):
     print('Are you sure you want to delete '+name+' forever?')
@@ -43,9 +46,26 @@ def get_confirm(name):
 
 def editing_menu_with_backup(subdictionary,prompt_string,options_list):
     backup = json.loads(json.dumps(subdictionary))
+    valid_choices = set()
+    for index,name in enumerate(options_list):
+        valid_choices.add(str(index+3))
     print('-------------------------------------------------------------------')
     while True:
         print(prompt_string)
         print('1. Cancel and return to previous menu')
-        print('2. Save and return to previous')
-        
+        print('2. Save and return to previous menu')
+        for index,value in enumerate(options_list):
+            print('{0}. {1}'.format(index+3,value[0]))
+        choice = input('Enter the number corresponding to your selection: ')
+        if choice == '1': return backup
+        if choice == '2': return subdictionary
+        if choice in valid_choices:
+            options_list[int(choice)-3][1](subdictionary)
+            print('-------------------------------------------------------------------')
+            continue
+        print('-------------------------------------------------------------------')
+        print('Error, invalid input')
+
+a = {1:True,2:False}
+a = editing_menu_with_backup(a,'Pick an option',[('Add 3-True',lambda x: x.update({3:True} or x)),('Toggle 1',lambda x: x.update({1:not x[1]} or x))])
+print(a)
