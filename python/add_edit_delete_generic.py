6be7c43c221e721abdcc6c491eea726ce9385b9d
Sam Patriquin <samuel_patriquin@student.uml.edu>
2019-06-25 15:57:45 -0400

    More generic function stuff

--- python/add_edit_delete_generic.py
+++ python/add_edit_delete_generic.py
@@ -1,5 +1,8 @@
 #This file contains functions that will act as generic helpers to the menu system
 
+
+#Pre: sub_dictionary is a valid subdict of the current configuration that is being modified, type is a valid string
+#Post: sub_dictionary has a new key value pair and is returned to the caller
 def generic_add(sub_dictionary, type):
 	key = get_regex_input(r'^([a-zA-Z0-9_\-\(\)])+$',
 				'Please enter a new' + type + 'key: ',
@@ -8,6 +11,7 @@ def generic_add(sub_dictionary, type):
 				'Please enter the value for' + type + ': ',
 				'Invalid input, please enter only numeric characters')
 	sub_dictionary[key] = value
+	return sub_dictionary
 
 def generic_delete(sub_dictionary, type):
 	
