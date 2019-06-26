f65871ed79a5663816d03f28ab0d02427b12d103
Sam Patriquin <samuel_patriquin@student.uml.edu>
2019-06-26 12:46:23 -0400

    Generic add edit and delete done

--- python/add_edit_delete_generic.py
+++ python/add_edit_delete_generic.py
@@ -6,12 +6,25 @@
 def generic_add(sub_dictionary, type):
 	key = get_regex_input(r'^([a-zA-Z0-9_\-\(\)])+$',
 				'Please enter a new' + type + 'key: ',
-				'Invalid input, please enter only alphanumeric characters underscores dashes or parthesis')
+				'Invalid input, please enter only alphanumeric characters underscores dashes or parenthesis')
 	value = get_regex_input(r'^([0-9])+$',
 				'Please enter the value for' + type + ': ',
 				'Invalid input, please enter only numeric characters')
 	sub_dictionary[key] = value
 	return sub_dictionary
 
-def generic_delete(sub_dictionary, type):
-	
+
+def generic_delete(sub_dictionary, name):
+	if(get_confirm("Are you sure you want to delete " + name + "?")):
+		del sub_dictionary[name]
+		print('Successfully deleted ' + name)
+		return
+	print('Deletion canceled')
+	return
+
+
+def generic_edit(sub_dictionary, name):
+	value = get_regex_input(r'^([a-zA-Z0-9_\-\(\)])+$',
+				'Please enter the new value for ' + name + ': ',
+				'Invalid input, please enter only alphanumeric characters underscores dashes or parenthesis'')
+	sub_dictionary[name] = value
